fd447ec6d835fabbd4f4d9039f8b98686554d9ba
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-21 22:08:45 +0530

    Auto Push

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ module "vpc" {
   ENV = var.ENV
   PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
   PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+  AZ =   var.AZ
 }
 
 # This always goes with the main branch, so how can I tell the module to pick the code from intended branch
