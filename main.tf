4d7415c33538a550777c287c1c000984b7b15cd2
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-21 10:15:21 +0530

    Auto Push

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
   source = "vendor/modules/vpc/"
+  VPC_CIDR = var.VPC_CIDR
 }
 
 # This always goes with the main branch, so how can I tell the module to pick the code from intended branch
