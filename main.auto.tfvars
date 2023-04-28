4d9d7a7db9de7baf8d8fa29639b48bd28aff4625
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-28 22:01:57 +0530

    Auto Push

--- main.auto.tfvars
+++ main.auto.tfvars
@@ -1,5 +1,8 @@
-AZ                      = ["us-east-1a", "us-east-1b"]
-DEFAULT_VPC_ID          = "vpc-0ace177c971986a50"
-DEFAULT_VPC_CIDR        = "172.31.0.0/16"
-DEFAULT_VPC_RT          = "rtb-0a9aa7cd990751ab5"
-PRIVATE_HOSTED_ZONE_ID  =  "Z03162902ZBUWVJLMZQNI"
+AZ                          = ["us-east-1a", "us-east-1b"]
+DEFAULT_VPC_ID              = "vpc-0ace177c971986a50"
+DEFAULT_VPC_CIDR            = "172.31.0.0/16"
+DEFAULT_VPC_RT              = "rtb-0a9aa7cd990751ab5"
+PRIVATE_HOSTED_ZONE_ID      =  "Z03162902ZBUWVJLMZQNI"
+PRIVATE_HOSTED_ZONE_NAME    = "roboshop.internal"
+PUBLIC_HOSTED_ZONE_ID      =  ""
+PUBLIC_HOSTED_ZONE_NAME    = ""
--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
 module "vpc" {
-  source                 = "./vendor/modules/vpc"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
-  VPC_CIDR               = var.VPC_CIDR
-  ENV                    = var.ENV
-  PUBLIC_SUBNET_CIDR     = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR    = var.PRIVATE_SUBNET_CIDR 
-  AZ                     = var.AZ
-  DEFAULT_VPC_ID         = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR       = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT         = var.DEFAULT_VPC_RT
-  PRIVATE_HOSTED_ZONE_ID = var.PRIVATE_HOSTED_ZONE_ID
+  source                    = "./vendor/modules/vpc"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+  VPC_CIDR                  = var.VPC_CIDR
+  ENV                       = var.ENV
+  PUBLIC_SUBNET_CIDR        = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR       = var.PRIVATE_SUBNET_CIDR 
+  AZ                        = var.AZ
+  DEFAULT_VPC_ID            = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_CIDR          = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_RT            = var.DEFAULT_VPC_RT
+  PRIVATE_HOSTED_ZONE_ID    = var.PRIVATE_HOSTED_ZONE_ID
+  PRIVATE_HOSTED_ZONE_NAME  = var.PRIVATE_HOSTED_ZONE_NAME
+  PUBLIC_HOSTED_ZONE_ID     = var.PUBLIC_HOSTED_ZONE_ID
+  PUBLIC_HOSTED_ZONE_NAME   = var.PUBLIC_HOSTED_ZONE_NAME
 }
 # This always goes with the main branch, so how can I tell the module to pick the code from intended branch
